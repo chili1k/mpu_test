bd34471909f98315df4c1e8eefed1a2dcf0ace17
Mitja Bezensek <bezensek.mitja@gmail.com>
2018-05-31 21:24:27 +0200

    More careful code.

--- mpu_test.ino
+++ mpu_test.ino
@@ -1,8 +1,8 @@
-#include<Wire.h>
 #include <Bounce2.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 
-#define MPU_ADDR 0x68  // I2C address of the MPU-6050
-#define MAX_ITERS 100
+#define INTERRUPT_PIN 2
 #define PIN_BUTTON_1 3
 #define PIN_BUTTON_2 4
 #define PRINT_DELAY 1000
@@ -10,7 +10,19 @@
 #define PRINT_CSV_ITEM(x) Serial.print(x); Serial.print(",");
 #define PRINT_LAST_CSV_ITEM(x) Serial.print(x); Serial.println();
 
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+MPU6050 mpu;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+VectorFloat gravity;    // [x, y, z]            gravity vector
+Quaternion q;           // [w, x, y, z]         quaternion container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
 Bounce button1;
 Bounce button2;
 
@@ -20,15 +32,12 @@ unsigned long time = 0;
 bool wasButton1Pushed;
 bool wasButton2Pushed;
 
-void setup(){
-  Wire.begin();
-  Wire.setClock(100000);
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
-  Serial.begin(115200);
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
 
+void setup() {
   pinMode(PIN_BUTTON_1, INPUT);
   button1.attach(PIN_BUTTON_1);
   button1.interval(20);
@@ -37,61 +46,70 @@ void setup(){
   button2.attach(PIN_BUTTON_2);
   button2.interval(20);
 
+
+  Wire.begin();
+  Wire.setClock(100000); 
+  mpu.initialize();
+
+  Serial.begin(115200);
+  Serial.println(F("Initializing I2C devices..."));
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  //mpu.setXAccelOffset(-4816);
+  //mpu.setYAccelOffset(-8075);
+  //mpu.setZAccelOffset(-20331);
+  //mpu.setXGyroOffset(-580);
+  //mpu.setYGyroOffset(-37);
+  //mpu.setZGyroOffset(-81);
+
+//  mpu.setXGyroOffset(220);
+//  mpu.setYGyroOffset(76);
+//  mpu.setZGyroOffset(-85);
+//  mpu.setZAccelOffset(1688);
+  
+  if (devStatus == 0) {
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    Serial.println(F("DMP ready!"));
+    dmpReady = true;
+
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
   // csv header
-  Serial.println("Tm,Btn1,Btn2,AcX,AcY,AcZ,GyX,GyY,GyZ");
-}
-
-void printCsvItem(bool isLastItem) {
-  Serial.print(AcX);
-  Serial.print(",");
-}
-
-void printLastCsvItem() {
-  Serial.print(AcX);
-  Serial.println();
+  Serial.println("Tm,Btn1,Btn2,yaw,pitch,roll,qw,qx,qy,qz");
 }
 
 void printGyro() {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR,14,true);  // request a total of 14 registers
-
-  AcX=AcY=AcZ=Tmp=GyX=GyY=GyZ=0;
-  for (int i = 0; i < MAX_ITERS; i++) {
-    AcX+=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-    AcY+=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    AcZ+=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    Tmp+=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    GyX+=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    GyY+=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    GyZ+=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  }
-
-  AcX/=MAX_ITERS;
-  AcY/=MAX_ITERS;
-  AcZ/=MAX_ITERS;
-  Tmp/=MAX_ITERS;
-  GyX/=MAX_ITERS;
-  GyY/=MAX_ITERS;
-  GyZ/=MAX_ITERS;
-
   int btn1 = wasButton1Pushed ? 1 : 0;
   int btn2 = wasButton2Pushed ? 1 : 0;
-
   PRINT_CSV_ITEM(time);
   PRINT_CSV_ITEM(btn1);
   PRINT_CSV_ITEM(btn2);
-  PRINT_CSV_ITEM(AcX);
-  PRINT_CSV_ITEM(AcY);
-  PRINT_CSV_ITEM(AcZ);
-  //PRINT_CSV_ITEM(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  PRINT_CSV_ITEM(GyX);
-  PRINT_CSV_ITEM(GyY);
-  PRINT_LAST_CSV_ITEM(GyZ);
+  PRINT_CSV_ITEM(ypr[0]);
+  PRINT_CSV_ITEM(ypr[1]);
+  PRINT_CSV_ITEM(ypr[2]);
+  PRINT_CSV_ITEM(q.w);
+  PRINT_CSV_ITEM(q.x);
+  PRINT_CSV_ITEM(q.y);
+  PRINT_LAST_CSV_ITEM(q.z);
 }
 
-void loop(){
+void loop() {
+  if (!dmpReady) return;
+
   button1.update();
   button2.update();
   // if button was pressed at any point during the last interval
@@ -107,5 +125,32 @@ void loop(){
     wasButton1Pushed = false;
     wasButton2Pushed = false;
     time++;
+  }
+
+  // no data yet
+  if (!mpuInterrupt) {
+    return;
+  }
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  fifoCount = mpu.getFIFOCount();
+
+  if (fifoCount == 1024) {
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+  } else { 
+    if (fifoCount % packetSize != 0) {
+      mpu.resetFIFO();
+//      Serial.println(F("Packet is corrupted!"));
+    } else {
+      while (fifoCount >= packetSize) {
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+      }
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    }
   }
 }
